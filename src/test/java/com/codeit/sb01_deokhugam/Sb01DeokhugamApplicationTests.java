846eb52ffd2a4043c0b07a75edfdac934c228a9f
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-17 18:35:31 +0900

    :tada: Spring boot project init

---
+++ src/test/java/com/codeit/sb01_deokhugam/Sb01DeokhugamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.sb01_deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb01DeokhugamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
