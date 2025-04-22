e0369c7b257636aaecda0961b7b1184e5957b530
kaya <answerme0622@daum.net>
2025-04-22 21:07:15 +0900

    :sparkles: User 요청/응답 DTO 설계 #42

---
+++ src/main/java/com/codeit/sb01_deokhugam/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codeit.sb01_deokhugam.domain.user.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codeit.sb01_deokhugam.domain.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+	boolean existsByUsername(String username);
+
+	
+	
+}
