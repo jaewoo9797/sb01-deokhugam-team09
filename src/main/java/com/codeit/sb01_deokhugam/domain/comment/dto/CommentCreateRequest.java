99f0905067333068691f3e85ddbcbf0000dfc66c
뺑열 <byeongyeol12@gmail.com>
2025-04-22 15:10:08 +0900

    fix: 댓글 생성 및 수정 DTO에 @NotBlank @NotNull 추가

--- src/main/java/com/codeit/sb01_deokhugam/domain/comment/dto/CommentCreateRequest.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/comment/dto/CommentCreateRequest.java
@@ -1,9 +1,17 @@
 package com.codeit.sb01_deokhugam.domain.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record CommentCreateRequest (
+        @NotNull(message = "리뷰 ID는 필수 입력 항목입니다.")
         UUID reviewID,
+
+        @NotNull(message = "사용자 ID는 필수 입력 항목입니다.")
         UUID userId,
+
+        @NotBlank(message = "댓글 내용은 필수 입력 항목입니다.")
         String content
 ) {}
