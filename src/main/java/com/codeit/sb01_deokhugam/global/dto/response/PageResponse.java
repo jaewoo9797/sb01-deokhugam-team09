71513abd3057787ad69e7d56e1eb26f44ddcc49a
hyojkim2 <skql2548@gmail.com>
2025-04-22 18:59:58 +0900

    :sparkles: 페이지 리스폰스 작성- nextAfter 추가 #51

--- src/main/java/com/codeit/sb01_deokhugam/global/dto/response/PageResponse.java
+++ src/main/java/com/codeit/sb01_deokhugam/global/dto/response/PageResponse.java
@@ -1,9 +1,11 @@
 package com.codeit.sb01_deokhugam.global.dto.response;
 
+import java.time.Instant;
 import java.util.List;
 
 public record PageResponse<T>(
 	List<T> content,
+	Instant nextAfter,
 	Object nextCursor,
 	int size,
 	boolean hasNext,
@@ -11,3 +13,4 @@ public record PageResponse<T>(
 ) {
 
 }
+
