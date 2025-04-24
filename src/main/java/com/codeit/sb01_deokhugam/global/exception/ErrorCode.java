1f12c5399af35340210286ce5637df7cb5abd62a
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-24 10:24:44 +0900

    :sparkles: Feature: S3 파일 업로드 기능 및 예외 처리 로직 추가

--- src/main/java/com/codeit/sb01_deokhugam/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/sb01_deokhugam/global/exception/ErrorCode.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 	//global
-	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."), INVALID_REQUEST("잘못된 요청입니다"), ILLEGAL_ARGUMENT_ERROR(
-		"잘못된 인수가 전달되었습니다."),
+	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
+	INVALID_REQUEST("잘못된 요청입니다"),
+	ILLEGAL_ARGUMENT_ERROR("잘못된 인수가 전달되었습니다."),
 
 	//book
 	BOOK_NOT_FOUND("해당 도서가 존재하지 않습니다."),
@@ -24,7 +25,10 @@ public enum ErrorCode {
 
 	//user
 	USER_NOT_FOUND("해당 유저가 존재하지 않습니다."),
-	;
+
+	//s3
+	S3_UPLOAD_ERROR("S3 업로드 중 오류가 발생했습니다."),
+	FILE_NAME_MISSING("파일 이름이 유효하지 않습니다.");
 
 	private final String message; //메시지
 
