054022babb5cde8721f691710cc0999c016e563d
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-25 12:57:58 +0900

    :sparkles: Feature: Implement NotificationController with methods to update and retrieve notifications

---
+++ src/main/java/com/codeit/sb01_deokhugam/domain/notification/controller/NotificationController.java
@@ -0,0 +1,44 @@
+package com.codeit.sb01_deokhugam.domain.notification.controller;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codeit.sb01_deokhugam.domain.notification.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+	private final NotificationService notificationService;
+
+	@PatchMapping("/{id}")
+	public void updateConfirm(@PathVariable UUID id) {
+
+	}
+
+	@PatchMapping("/read-all")
+	public void updateConfirmAll() {
+
+	}
+
+	@GetMapping
+	public void getNotifications(
+		@RequestParam(name = "userId") UUID userId,
+		@RequestParam(defaultValue = "DESC") String direction,
+		@RequestParam(required = false) Instant cursor,
+		@RequestParam(required = false) Instant after,
+		@RequestParam(defaultValue = "20") int limit
+	) {
+
+	}
+}
