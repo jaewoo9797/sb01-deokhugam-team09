a27551b757d3697ff5b72bada0b505fb4a0d1029
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-25 14:16:48 +0900

    :sparkles: Feature: Update updateConfirm method to return NotificationDto and respond with notification confirmation

--- src/main/java/com/codeit/sb01_deokhugam/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/notification/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.codeit.sb01_deokhugam.domain.notification.controller;
 import java.time.Instant;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codeit.sb01_deokhugam.domain.notification.dto.request.NotificationUpdateRequest;
+import com.codeit.sb01_deokhugam.domain.notification.dto.response.NotificationDto;
 import com.codeit.sb01_deokhugam.domain.notification.service.NotificationService;
 import com.codeit.sb01_deokhugam.global.resolver.annotation.LoginUserId;
 
@@ -25,8 +27,9 @@ public class NotificationController {
 	private final NotificationService notificationService;
 
 	@PatchMapping("/{id}")
-	public void updateConfirm(@PathVariable UUID id, @RequestBody NotificationUpdateRequest request, @LoginUserId UUID userId) {
-		notificationService.confirmNotification(id, userId);
+	public ResponseEntity<NotificationDto> updateConfirm(@PathVariable UUID id, @RequestBody NotificationUpdateRequest request, @LoginUserId UUID userId) {
+		NotificationDto notificationDto = notificationService.confirmNotification(id, userId);
+		return ResponseEntity.ok(notificationDto);
 	}
 
 	@PatchMapping("/read-all")
