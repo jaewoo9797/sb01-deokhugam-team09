c50d928c15e9c645abf65f6adc93189cddac44da
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-25 13:24:48 +0900

    :sparkles: Feature: Add NotificationException class and update NotificationRepository to use UUIDs

--- src/main/java/com/codeit/sb01_deokhugam/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/notification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.codeit.sb01_deokhugam.domain.notification.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,6 @@ import com.codeit.sb01_deokhugam.domain.notification.entity.Notification;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 	@Query(value = "SELECT n FROM Notification n WHERE n.id= :id AND n.user.id = :userId")
-	Optional<Notification> findByIdAndUserId(Long id, Long userId);
+	Optional<Notification> findByIdAndUserId(UUID id, UUID userId);
 
 }
