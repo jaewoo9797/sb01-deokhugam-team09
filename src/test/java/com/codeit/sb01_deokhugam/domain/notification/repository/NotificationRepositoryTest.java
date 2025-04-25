800f8947fa1f5333103b9118044b4edb77e0ef8d
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-25 14:43:45 +0900

    :sparkles: Feature: Add NotificationRepositoryTest for repository layer testing

---
+++ src/test/java/com/codeit/sb01_deokhugam/domain/notification/repository/NotificationRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.codeit.sb01_deokhugam.domain.notification.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class NotificationRepositoryTest {
+
+}
