46c7b1f77103e15275ca0ae8375752cb261b4df6
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-25 14:15:22 +0900

    :sparkles: Feature: Update updateConfirm method to accept NotificationUpdateRequest and LoginUserId for notification confirmation

--- src/main/java/com/codeit/sb01_deokhugam/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/notification/controller/NotificationController.java
@@ -6,11 +6,14 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codeit.sb01_deokhugam.domain.notification.dto.request.NotificationUpdateRequest;
 import com.codeit.sb01_deokhugam.domain.notification.service.NotificationService;
+import com.codeit.sb01_deokhugam.global.resolver.annotation.LoginUserId;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +25,8 @@ public class NotificationController {
 	private final NotificationService notificationService;
 
 	@PatchMapping("/{id}")
-	public void updateConfirm(@PathVariable UUID id) {
-
+	public void updateConfirm(@PathVariable UUID id, @RequestBody NotificationUpdateRequest request, @LoginUserId UUID userId) {
+		notificationService.confirmNotification(id, userId);
 	}
 
 	@PatchMapping("/read-all")
