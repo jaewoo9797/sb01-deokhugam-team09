bbcbf0f18f92f9f2e336a25c36544034b023ff64
kaya <answerme0622@daum.net>
2025-04-28 11:22:44 +0900

    :sparkles: 유저랭킹을 저장할 PowerUser 엔티티 추가 #81

--- src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/entity/PowerUser.java
+++ src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/entity/PowerUser.java
@@ -1,5 +1,45 @@
 package com.codeit.sb01_deokhugam.ranking.poweruser.entity;
 
-public class PowerUser {
-	
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import com.codeit.sb01_deokhugam.domain.base.BaseEntity;
+import com.codeit.sb01_deokhugam.global.enumType.Period;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user_ranking")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PowerUser extends BaseEntity {
+
+	@Column(name = "user_id", nullable = false)
+	private UUID userId;
+
+	@Column(name = "nickname", nullable = false)
+	private String nickname;
+
+	@Column(name = "period", nullable = false)
+	Period period;
+
+	@Column(name = "rank", nullable = false)
+	int rank;
+
+	@Column(name = "score", precision = 2, scale = 1)
+	BigDecimal score;
+
+	@Column(name = "review_score_sum", precision = 2, scale = 1)
+	BigDecimal reviewScoreSum;
+
+	@Column(name = "like_count", nullable = false)
+	int likeCount;
+
+	@Column(name = "comment_count", nullable = false)
+	int commentCount;
 }
