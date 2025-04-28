77a5bdc1d9b3213114545e4e81934899adcdeec7
kaya <answerme0622@daum.net>
2025-04-28 17:34:00 +0900

    :sparkles: 페이지네이션을 활용한 파워유저 랭킹조회 기능 추가 #81

--- src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/mapper/PowerUserMapper.java
+++ src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/mapper/PowerUserMapper.java
@@ -1,11 +1,19 @@
 package com.codeit.sb01_deokhugam.ranking.poweruser.mapper;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
+import com.codeit.sb01_deokhugam.global.dto.response.PageResponse;
 import com.codeit.sb01_deokhugam.ranking.poweruser.dto.response.PowerUserDto;
 import com.codeit.sb01_deokhugam.ranking.poweruser.entity.PowerUser;
 
 @Mapper(componentModel = "spring")
 public interface PowerUserMapper {
 	PowerUserDto toDto(PowerUser powerUser);
+
+	PageResponse<PowerUserDto> toPageResponseDto(List<PowerUser> powerUsers, Instant nextAfter, int nextCursor,
+		int size, boolean hasNext, long totalElements);
+
 }
