0e29b5c321c0662c0b21258b666dd35465d9260a
hyojkim2 <skql2548@gmail.com>
2025-04-29 15:04:23 +0900

    :bug: BookMapper 어노테이션 제거 #77

--- src/main/java/com/codeit/sb01_deokhugam/domain/book/mapper/BookMapper.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/book/mapper/BookMapper.java
@@ -1,13 +1,11 @@
 package com.codeit.sb01_deokhugam.domain.book.mapper;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
 import com.codeit.sb01_deokhugam.domain.book.dto.BookDto;
 import com.codeit.sb01_deokhugam.domain.book.entity.Book;
 
 @Mapper(componentModel = "spring")
-@Component
 public interface BookMapper {
 
 	BookDto toDto(Book book);
