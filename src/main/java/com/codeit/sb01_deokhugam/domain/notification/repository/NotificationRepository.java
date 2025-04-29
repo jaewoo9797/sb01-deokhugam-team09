6ff3dfa7e5dae9abd62c1bf3cdf95d7f5c6b598e
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-29 14:34:48 +0900

    :recycle: Refactor: Enhance notification update method to flush changes automatically and add existence check for unconfirmed notifications

--- src/main/java/com/codeit/sb01_deokhugam/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/notification/repository/NotificationRepository.java
@@ -16,9 +16,12 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 	@Query(value = "SELECT n FROM Notification n JOIN FETCH n.user u JOIN FETCH n.review r WHERE n.id = :id AND n.user.id = :userId")
 	Optional<Notification> findByIdAndUserId(@Param("id") UUID id, @Param("userId") UUID userId);
 
-	@Modifying(clearAutomatically = true)
-	@Query("UPDATE Notification n SET n.confirmed = true WHERE n.user.id = :userId AND n.confirmed = false")
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query("UPDATE Notification n SET n.confirmed = TRUE WHERE n.user.id = :userId AND n.confirmed = false")
 	int updateAllConfirmed(@Param("userId") UUID userId);
 
 	List<Notification> findAllByUserId(UUID userId);
+
+	// 알림이 확인되지 않고 계속해서 쌓여간다면 조회 시 복합 인덱스 고려
+	boolean existsByUserIdAndConfirmedFalse(UUID userId);
 }
