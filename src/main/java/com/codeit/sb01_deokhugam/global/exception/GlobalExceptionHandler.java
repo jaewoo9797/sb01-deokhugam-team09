c825f0e8c77233059d83128859f25a0d1a8333a6
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-30 10:48:17 +0900

    feat: Add exception handlers for method argument type mismatch, unsupported media type, and unsupported HTTP method in GlobalExceptionHandler

--- src/main/java/com/codeit/sb01_deokhugam/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/sb01_deokhugam/global/exception/GlobalExceptionHandler.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,6 +32,30 @@ public class GlobalExceptionHandler {
 		log.error("필수 요청 파라미터가 누락되었습니다:  message = {}", exception.getMessage());
 		ErrorResponse errorResponse = new ErrorResponse(exception, HttpStatus.BAD_REQUEST.value());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	}
+
+	// 쿼리스트링/PathVariable → 타입 매핑 실패(enum, 숫자 등)
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
+		log.error("Type Mapping Fail 잘못된 인수가 전달되었습니다:  message = {}", exception.getMessage());
+		ErrorResponse errorResponse = new ErrorResponse(exception, HttpStatus.BAD_REQUEST.value());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	}
+
+	// 지원하지 않는 Content-Type
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException exception) {
+		log.error("지원하지 않는 Content-Type 입니다:  message = {}", exception.getMessage());
+		ErrorResponse errorResponse = new ErrorResponse(exception, HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(errorResponse);
+	}
+
+	// 지원하지 않는 HTTP 메소드 요청 시
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+		log.error("지원하지 않는 HTTP 메소드 요청입니다:  message = {}", exception.getMessage());
+		ErrorResponse errorResponse = new ErrorResponse(exception, HttpStatus.METHOD_NOT_ALLOWED.value());
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
 	}
 
 	/**
