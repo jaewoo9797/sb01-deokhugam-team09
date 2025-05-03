bd4ca2f193e64ddd4b8143919503f3d3e382a64c
kaya <answerme0622@daum.net>
2025-05-03 17:32:55 +0900

    :sparkles: 유저 생성, 정보수정 테스트 메서드 추가 #96

--- src/test/java/com/codeit/sb01_deokhugam/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/codeit/sb01_deokhugam/domain/user/controller/UserControllerTest.java
@@ -55,7 +55,7 @@ public class UserControllerTest {
 			"nickname", TEST_NICKNAME,
 			"password", TEST_PASSWORD
 		);
-
+		//when & then
 		given().log().all()
 			.contentType(ContentType.JSON)
 			.body(requestBody)
@@ -167,16 +167,39 @@ public class UserControllerTest {
 		);
 	}
 
+	@DisplayName("유저 생성 실패 테스트 - 이미 존재하는 이메일로 회원가입 요청")
+	@Test
+	void create_user_when_fail_existent_email_then_return_status_409() {
+		User user = insurtTestUser();
+		//given
+		String existingEmail = user.getEmail();
+		Map<String, String> requestBody = Map.of("email", existingEmail, "nickname", "user", "password",
+			"password486!!");
+		//when
+		ExtractableResponse<Response> response = given().log().all()
+			.contentType(ContentType.JSON)
+			.body(requestBody)
+			.when().post("/api/users")
+			.then().log().all()
+			.statusCode(409)
+			.extract();
+		//then
+		final JsonPath result = response.jsonPath();
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CONFLICT.value()),
+			() -> assertThat(result.getString("message")).isEqualTo("이미 존재하는 사용자입니다."),
+			() -> assertThat(result.getString("code")).isEqualTo("DUPLICATION_USER"),
+			() -> assertThat(result.getString("timestamp")).isNotNull()
+		);
+	}
+
 	@DisplayName("유저 조회 성공 테스트")
 	@Test
 	void find_user_return_status_200() {
 		User user = insurtTestUser();
-		userRepository.save(user);
-
 		//given
 		UUID userId = user.getId();
-
-		//when
+		//when & then
 		given().log().all()
 			.when().get("/api/users/" + userId.toString())
 			.then().log().all()
@@ -187,26 +210,23 @@ public class UserControllerTest {
 			.body("createdAt", notNullValue());
 	}
 
+	// todo 영속성 컨텍스트 초기화 로직 고려
 	@DisplayName("유저 조회 실패 테스트 - 논리삭제된 유저 조회")
 	@Test
 	void find_user_when_soft_deleted_user_then_return_status_404() {
 		User user = insurtTestUser();
 		user.softDelete();
-		userRepository.save(user);  // DB에 변경사항 저장
-
+		userRepository.save(user);
 		//given
 		UUID userId = user.getId();
-
 		//when
 		ExtractableResponse<Response> response = given().log().all()
 			.when().get("/api/users/" + userId.toString())
 			.then().log().all()
 			.statusCode(404)
 			.extract();
-
 		//then
 		final JsonPath result = response.jsonPath();
-		Map<String, String> details = result.getMap("details");
 		assertAll(
 			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
 			() -> assertThat(result.getString("message")).isEqualTo("사용자를 찾을 수 없습니다."),
@@ -220,16 +240,14 @@ public class UserControllerTest {
 	void find_user_when_nonexistent_user_then_return_status_404() {
 		//given
 		UUID nonexistentUserId = UUID.randomUUID();
-
 		//when
 		ExtractableResponse<Response> response = given().log().all()
 			.when().get("/api/users/" + nonexistentUserId.toString())
 			.then().log().all()
 			.statusCode(404)
 			.extract();
-
+		//then
 		final JsonPath result = response.jsonPath();
-		Map<String, String> details = result.getMap("details");
 		assertAll(
 			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
 			() -> assertThat(result.getString("message")).isEqualTo("사용자를 찾을 수 없습니다."),
@@ -238,10 +256,97 @@ public class UserControllerTest {
 		);
 	}
 
-	@DisplayName("유저 정보 수정 테스트")
+	@DisplayName("유저 정보수정 성공 테스트")
 	@Test
-	void update() {
+	void update_user_return_status_200() {
+		User user = insurtTestUser();
+		//given
+		Map<String, String> requestBody = Map.of("nickname", "newNickname");
+		UUID userId = user.getId();
+		//when & then
+		given().log().all()
+			.contentType(ContentType.JSON)
+			.header("deokhugam-request-user-id", userId)
+			// 헤더추가
+			.body(requestBody)
+			.when().patch("/api/users/" + userId.toString())
+			.then().log().all()
+			.statusCode(200)
+			.body("nickname", equalTo("newNickname"));
+	}
 
+	@DisplayName("유저 정보수정 실패 테스트 - 형식에 맞지 않는 닉네임으로 수정 시도")
+	@Test
+	void update_user_when_invalid_formated_field_then_return_status_400() {
+		User user = insurtTestUser();
+		//given
+		Map<String, String> requestBody = Map.of("nickname", "n");
+		UUID userId = user.getId();
+		ExtractableResponse<Response> response = given().log().all()
+			.contentType(ContentType.JSON)
+			.header("deokhugam-request-user-id", userId)
+			.body(requestBody)
+			.when().patch("/api/users/" + userId.toString())
+			.then().log().all()
+			.statusCode(400)
+			.extract();
+		final JsonPath result = response.jsonPath();
+		Map<String, String> details = result.getMap("details");
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+			() -> assertThat(result.getString("message")).isEqualTo("요청 데이터가 유효하지 않습니다."),
+			() -> assertThat(result.getString("code")).isEqualTo("VALIDATION_ERROR"),
+			() -> assertThat(result.getString("timestamp")).isNotNull(),
+			() -> assertThat(details.get("nickname")).isEqualTo("닉네임은 2자 이상 20자 이하로 입력해주세요.")
+		);
+	}
+
+	@DisplayName("유저 정보수정 실패 테스트 - 요청 헤더의 id와 경로변수 불일치")
+	@Test
+	void update_user_when_header_id_differs_from_path_id_then_return_status_403() {
+		User user = insurtTestUser();
+		UUID userId = user.getId();
+		UUID wrongUserId = UUID.randomUUID();
+		Map<String, String> requestBody = Map.of("nickname", "newNickname");
+		ExtractableResponse<Response> response = given().log().all()
+			.contentType(ContentType.JSON)
+			.header("deokhugam-request-user-id", wrongUserId.toString())
+			.body(requestBody)
+			.when().patch("/api/users/" + userId.toString())
+			.then().log().all()
+			.statusCode(403)
+			.extract();
+		final JsonPath result = response.jsonPath();
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value()),
+			() -> assertThat(result.getString("message")).isEqualTo("접근 권한이 없습니다."),
+			() -> assertThat(result.getString("code")).isEqualTo("ACCESS_DENIED"),
+			() -> assertThat(result.getString("timestamp")).isNotNull()
+		);
+	}
+
+	@DisplayName("유저 정보수정 실패 테스트 - 존재하지 않는 유저 수정 시도")
+	@Test
+	void update_user_when_nonexistent_user_then_return_status_404() {
+		User user = insurtTestUser();
+		UUID userId = user.getId();
+		UUID wrongPathVariable = UUID.randomUUID();
+		Map<String, String> requestBody = Map.of("nickname", "newNickname");
+		ExtractableResponse<Response> response = given().log().all()
+			.contentType(ContentType.JSON)
+			.header("deokhugam-request-user-id", userId.toString())
+			.body(requestBody)
+			.when().patch("/api/users/" + wrongPathVariable.toString())
+			.then().log().all()
+			.statusCode(404)
+			.extract();
+		final JsonPath result = response.jsonPath();
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
+			() -> assertThat(result.getString("message")).isEqualTo("사용자를 찾을 수 없습니다."),
+			() -> assertThat(result.getString("code")).isEqualTo("USER_NOT_FOUND"),
+			() -> assertThat(result.getString("timestamp")).isNotNull()
+		);
 	}
 
 	@DisplayName("유저 소프트 삭제 테스트")
@@ -256,6 +361,8 @@ public class UserControllerTest {
 	}
 
 	User insurtTestUser() {
-		return EntityProvider.createUser();
+		User user = EntityProvider.createUser();
+		userRepository.save(user);
+		return user;
 	}
 }
