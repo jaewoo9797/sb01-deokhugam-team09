cec775007bc88d70aa53eb9f3b844bf66cdc7b43
hyojkim2 <skql2548@gmail.com>
2025-05-04 17:57:11 +0900

    :sparkles: 도서 랭킹 배치 연산을 위한 스케줄러 생성 #95

--- src/main/java/com/codeit/sb01_deokhugam/domain/book/service/BookRankingScheduler.java
+++ src/main/java/com/codeit/sb01_deokhugam/domain/book/service/BookRankingScheduler.java
@@ -16,11 +16,14 @@ public class BookRankingScheduler {
 	private final BookService bookService;
 
 	// 매일 연산 배치 수행
-	@Scheduled(cron = "0 2 * * * *")
+	@Scheduled(cron = "2 * * * * *")
 	public void updateDailyRanking() {
-		bookService.calculateRanking(Period.DAILY);
-		bookService.calculateRanking(Period.WEEKLY);
-		bookService.calculateRanking(Period.MONTHLY);
-		bookService.calculateRanking(Period.ALL_TIME);
+		//도서 랭킹 테이블을 비운다.
+		bookService.deleteBookRanking();
+
+		//period에 따라 도서 랭킹 계산 시작
+		for (Period period : Period.values()) {
+			bookService.calculateRanking(period);
+		}
 	}
 }
