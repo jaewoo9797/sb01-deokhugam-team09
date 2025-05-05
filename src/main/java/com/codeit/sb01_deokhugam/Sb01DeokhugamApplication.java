3f3b0e9ad52919ff829cb3de95b9c3aec73a7c30
hyojkim2 <skql2548@gmail.com>
2025-05-05 12:40:23 +0900

    :pencil2: 도서 서비스 주석 수정, application 스케줄링 어노테이션 삭제 #95

--- src/main/java/com/codeit/sb01_deokhugam/Sb01DeokhugamApplication.java
+++ src/main/java/com/codeit/sb01_deokhugam/Sb01DeokhugamApplication.java
@@ -2,10 +2,8 @@ package com.codeit.sb01_deokhugam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling //스케줄링 활성화
 public class Sb01DeokhugamApplication {
 
 	public static void main(String[] args) {
