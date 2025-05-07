888c67129764dcf0b344f78fa6b08805f6e3a378
hyojkim2 <skql2548@gmail.com>
2025-05-07 17:42:01 +0900

    :pencil2: naver api 주석처리  #111

--- src/main/java/com/codeit/sb01_deokhugam/global/naver/NaverBookClient.java
+++ src/main/java/com/codeit/sb01_deokhugam/global/naver/NaverBookClient.java
@@ -54,7 +54,7 @@ public class NaverBookClient {
 	}
 
 	private String get(String apiUrl, Map<String, String> requestHeaders) {
-		log.debug("apiUrl = {} 주소와 연결 시도", apiUrl);
+		//log.debug("apiUrl = {} 주소와 연결 시도", apiUrl);
 		HttpURLConnection con = connect(apiUrl);
 		try {
 			con.setRequestMethod("GET");
@@ -64,10 +64,10 @@ public class NaverBookClient {
 
 			int responseCode = con.getResponseCode();
 			if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
-				log.info("api = {} 가 정상 호출 되었습니다.", apiUrl);
+				//log.info("api = {} 가 정상 호출 되었습니다.", apiUrl);
 				return readBody(con.getInputStream());
 			} else { // 오류 발생
-				log.error("api = {} 오류 발생", apiUrl);
+				//log.error("api = {} 오류 발생", apiUrl);
 				return readBody(con.getErrorStream());
 			}
 		} catch (IOException e) {
