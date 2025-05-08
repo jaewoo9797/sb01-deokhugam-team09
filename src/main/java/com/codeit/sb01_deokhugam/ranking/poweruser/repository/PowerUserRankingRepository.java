048466c122ca3927c26b07b23cee1de46865d204
kaya <answerme0622@daum.net>
2025-05-09 00:29:46 +0900

    :bug: 배치데이터 네이티브쿼리 수정

--- src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/repository/PowerUserRankingRepository.java
+++ src/main/java/com/codeit/sb01_deokhugam/ranking/poweruser/repository/PowerUserRankingRepository.java
@@ -11,26 +11,30 @@ import org.springframework.data.repository.query.Param;
 import com.codeit.sb01_deokhugam.global.enumType.Period;
 import com.codeit.sb01_deokhugam.ranking.poweruser.entity.PowerUser;
 
-//todo 리뷰스코어썸 값 변경
 public interface PowerUserRankingRepository extends JpaRepository<PowerUser, UUID> {
 	@Query(value = """
 			SELECT
-				:period,
+				gen_random_uuid() as id,
+				CAST(:period AS VARCHAR) as period,
 				ROW_NUMBER() OVER (
 					ORDER BY score DESC, user_created_at ASC
 				) AS rank,
-				score,
-				review_score_sum,
-				like_count,
-				comment_count,
+				CAST(score AS NUMERIC(10,2)) AS score,
+				CAST(review_score_sum AS NUMERIC(10,2)) AS review_score_sum,
+				COALESCE(like_count, 0) AS like_count,
+				COALESCE(comment_count, 0) AS comment_count,
 				user_id,
-				nickname
+				nickname,
+				CURRENT_TIMESTAMP as created_at,
+				CURRENT_TIMESTAMP as updated_at
 			FROM (
 				SELECT
-					'WEEKLY' AS period,
-					(COALESCE(SUM(r.like_count), 0) * 0.5
-					 + COUNT(DISTINCT l.id) * 0.2
-					 + COUNT(DISTINCT c.id) * 0.3) AS score,
+					CAST(:period AS VARCHAR) AS period,
+					CAST(
+					COALESCE(SUM(r.like_count), 0) * 0.5
+				   	+ COUNT(DISTINCT l.id) * 0.2
+				   	+ COUNT(DISTINCT c.id) * 0.3
+				 	AS NUMERIC(10,2)) AS score,
 					COALESCE(SUM(rr.score), 0) AS review_score_sum,
 					COALESCE(SUM(r.like_count), 0) AS like_count,
 					COUNT(DISTINCT c.id) AS comment_count,
@@ -41,11 +45,12 @@ public interface PowerUserRankingRepository extends JpaRepository<PowerUser, UUI
 				LEFT JOIN reviews r
 					ON r.user_id = u.id AND r.created_at BETWEEN :start AND :end
 				LEFT JOIN review_rankings rr
-					ON rr.user_id = u.id AND rr.period = :period
+					ON rr.user_id = u.id AND rr.period = CAST(:period AS VARCHAR)
 				LEFT JOIN comments c
 					ON c.user_id = u.id AND c.created_at BETWEEN :start AND :end
 				LEFT JOIN review_likes l
 					ON l.user_id = u.id AND l.created_at BETWEEN :start AND :end
+				WHERE u.is_deleted = false
 				GROUP BY u.id, u.nickname, u.created_at
 			) AS ranked_users
 			ORDER BY rank;
@@ -55,6 +60,4 @@ public interface PowerUserRankingRepository extends JpaRepository<PowerUser, UUI
 		@Param("end") Instant end,
 		@Param("period") Period period
 	);
-
-	long countByPeriod(Period period);
 }
